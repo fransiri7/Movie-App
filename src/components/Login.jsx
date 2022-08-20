19520ced9cf52ab251e42457c99788a04c0719de
fransiri7 <81312079+fransiri7@users.noreply.github.com>
2022-08-20 19:00:28 -0300

    Validacion del formulario

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,32 @@
 function Login() {
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        const regExpresionEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        
+        if (email === "" || password === ""){
+            console.log ("Todos los campos deben ser obligatorios")
+            return;
+        }
+        if (email !== "" && !regExpresionEmail.test(email)){
+            console.log ("Debes ingresar un email valido")
+            return;
+        }
+        if (email !== "fransiri@gmail.com" && password !== "1234"){
+            console.log ("El mail o la contraseña no son correctos")
+        }
+
+        console.log("Ok, estamos listos para enviar la informacion")
+    }
+
     return (
         <div>
             <h2>Formulario de Login</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     <span>Correo electronico:</span><br/>
                     <input type="email" name="email" />
