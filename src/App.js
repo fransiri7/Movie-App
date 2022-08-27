50d79267abe7034269a5858af2f0812276da9071
fransiri7 <81312079+fransiri7@users.noreply.github.com>
2022-08-27 11:54:24 -0300

    Implementacion de los componentes Header y Footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Login from "./components/Login";
 import Listado from "./components/Listado";
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
     return (
        <div>
+        <Header/>
        <Routes>
             <Route exact path="/" element={<Login />} />
             <Route path="/listado" element={<Listado />} />
         </Routes>
+        <Footer/>
       </div>
     );
 }
