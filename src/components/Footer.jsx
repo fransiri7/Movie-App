c006833810783eb47eadac9a3f391984b5405821
fransiri7 <81312079+fransiri7@users.noreply.github.com>
2022-08-27 11:40:50 -0300

    Armado del footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer (){
+    return (
+     <footer>
+        <nav>
+            <ul>
+                <li>
+                    <a href="http://instagram.com" rel="noopener noreferrer"> IG </a>
+                </li>
+            </ul>
+        </nav>
+        <p>Copyright 2022</p>
+     </footer>
+    )
+
+}
+
+export default Footer;
