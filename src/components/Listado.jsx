e13d28d54d4b0ef5f3595ed5e23c1070570e6cb7
fransiri7 <81312079+fransiri7@users.noreply.github.com>
2022-08-28 17:53:28 -0300

    Renderizacion de datos en el componente

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,46 +1,47 @@
-import { Link, Navigate} from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 function Listado() {
- 
     const [moviesList, setMoviesList] = useState([]);
 
     useEffect(() => {
-        const endpoint = "https://api.themoviedb.org/3/discover/movie?api_key=379f6ca71eb017bd6e9f97929ced1fde&language=es-ES&page1"
-        axios.get(endpoint)
-            .then (response => {
-                const apiData = response.data.results;
-                setMoviesList(apiData)
-            })
+        const endpoint =
+            "https://api.themoviedb.org/3/discover/movie?api_key=379f6ca71eb017bd6e9f97929ced1fde&language=es-ES&page1";
+        axios.get(endpoint).then((response) => {
+            const apiData = response.data.results;
+            setMoviesList(apiData);
+        });
     }, [setMoviesList]);
 
-    console.log(moviesList)
+    console.log(moviesList);
 
     //token si no estas logueado es NULL
-    let token = localStorage.getItem("token")
+    let token = localStorage.getItem("token");
     return (
         <>
-        { !token && <Navigate to="/"/>}
-        <div className="row">
-            <div className="col-3">
-                <div className="card">
-                    <img src="..." className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">Movie title</h5>
-                        <p className="card-text">
-                            Review movie<br/> Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Quas rem iste dolor eos architecto maxime nihil sed minus reprehenderit
-                            doloribus. Officia, omnis. Porro beatae ex recusandae dolore voluptatum?
-                            Amet, blanditiis.
-                        </p>
-                        <Link to="/" className="btn btn-primary">
-                            View details
-                        </Link>
+            {!token && <Navigate to="/" />}
+
+            <div className="row">
+                {moviesList.map((movie, index) => {
+                    return(
+                    <div className="col-3 my-4" key={index}>
+                        <div className="card">
+                            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">{movie.title}</h5>
+                                <p className="card-text">
+                                    {movie.overview.substring(0,100)}...
+                                </p>
+                                <Link to="/" className="btn btn-primary">
+                                    View details
+                                </Link>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                    )
+                })}
             </div>
-        </div>
         </>
     );
 }
