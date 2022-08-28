f82dc15df145b26f0263c3454d62a37a8726dd65
fransiri7 <81312079+fransiri7@users.noreply.github.com>
2022-08-28 17:21:48 -0300

    Obtencion de datos desde la API

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,75 @@
 import { Link } from "react-router-dom";
 
-function Header (){
+function Header() {
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/listado">Listado</Link>
-                    </li>
-                    <li>
-                        <Link to="/contacto">Contacto</Link>
-                    </li>
-                </ul>
+        <>
+            <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">
+                        Movie App
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link active" aria-current="page" to="/">
+                                    Home
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/listado">
+                                    Listado
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/contacto">
+                                    Contacto
+                                </Link>
+                            </li>
+                            
+                        </ul>
+                        <form className="d-flex" role="search">
+                            <input
+                                className="form-control me-2"
+                                type="search"
+                                placeholder="Search"
+                                aria-label="Search"
+                            />
+                            <button className="btn btn-outline-success" type="submit">
+                                Search
+                            </button>
+                        </form>
+                    </div>
+                </div>
             </nav>
-        </header>
-    )
+        </>
+
+        // <header>
+        //     <nav>
+        //         <ul>
+        //             <li>
+        //                 <Link to="/">Home</Link>
+        //             </li>
+        //             <li>
+        //                 <Link to="/listado">Listado</Link>
+        //             </li>
+        //             <li>
+        //                 <Link to="/contacto">Contacto</Link>
+        //             </li>
+        //         </ul>
+        //     </nav>
+        // </header>
+    );
 }
 
 export default Header;
--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,15 +1,21 @@
-import { Link, useNavigate, Navigate} from "react-router-dom";
-import { useEffect } from "react";
+import { Link, Navigate} from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Listado() {
-    // const navigate = useNavigate();
+ 
+    const [moviesList, setMoviesList] = useState([]);
 
-    // useEffect(() => {
-    //     const token = localStorage.getItem("token");
-    //     if (token === null) {
-    //         navigate("/");
-    //     }
-    // }, [navigate]);
+    useEffect(() => {
+        const endpoint = "https://api.themoviedb.org/3/discover/movie?api_key=379f6ca71eb017bd6e9f97929ced1fde&language=es-ES&page1"
+        axios.get(endpoint)
+            .then (response => {
+                const apiData = response.data.results;
+                setMoviesList(apiData)
+            })
+    }, [setMoviesList]);
+
+    console.log(moviesList)
 
     //token si no estas logueado es NULL
     let token = localStorage.getItem("token")
