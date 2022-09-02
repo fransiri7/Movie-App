f5bb659447b7ca98aa078f298deb8f29de3ff3c9
fransiri7 <81312079+fransiri7@users.noreply.github.com>
2022-09-01 21:54:56 -0300

    Renderizar los datos de la API

--- src/components/Detalle.jsx
+++ src/components/Detalle.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 
 function Detalle (){
@@ -7,34 +7,45 @@ function Detalle (){
     let query = new URLSearchParams(window.location.search);
     let movieID = query.get("movieID");
 
+    const [movie, setMovie] = useState(null);
+
     useEffect(()=>{
         const responseDetail = `https://api.themoviedb.org/3/movie/${movieID}?api_key=379f6ca71eb017bd6e9f97929ced1fde&language=es-ES`
         axios.get(responseDetail)
         .then((response)=>{
-            const dataDetail = response.data
+            const movieData = response.data
+            setMovie(movieData)
+        })
+        .catch(error => {
+            console.log(error, "Hubo erroes, intenta mas tarde")
         })
     }, [])
 
     return (
         <>
         {!token && <Navigate to="/" />}
-        <h2>Titulo: titulo de la peli</h2>
-        <div className="row">
-            <div className="col-4">
-                imagen de la peli
+        { !movie && <p>Cargando ... </p>}
+        { movie &&
+        <>
+            <h2>Titulo: {movie.title}</h2>
+            <div className="row">
+                <div className="col-4">
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} className="img-fluid" alt="movie-poster" />
+                </div>
+                <div className="col-8">
+                    <h5>Fecha de estreno: {movie.release_date}</h5>
+                    <h5>Reseña:</h5>
+                    <p>{movie.overview}</p>
+                    <h5>Generos:</h5>
+                    {movie.genres.map((el)=>{
+                        return (
+                           <li key={el.id}>{el.name}</li>
+                        )
+                    })}
+                </div>
             </div>
-            <div className="col-8">
-                <h5>Fecha de estreno:</h5>
-                <h5>Reseña:</h5>
-                <p>El detalle de la pelicula, contame de q se trata</p>
-                <h5>Generos:</h5>
-                <ul>
-                    <li>Genero 1</li>
-                    <li>Genero 2</li>
-                    <li>Genero 3</li>
-                </ul>
-            </div>
-        </div>
+        </>
+        }
         </>
     )
 }
