b224af6201232603f11cdc2cefa26d93d30029f5
fransiri7 <81312079+fransiri7@users.noreply.github.com>
2022-09-01 21:04:18 -0300

    Armado de la vista 'Detalle'

--- src/components/Detalle.jsx
+++ src/components/Detalle.jsx
@@ -1,12 +1,36 @@
 import { Navigate } from "react-router-dom";
+import {useEffect} from "react";
+import axios from "axios";
 
 function Detalle (){
     let token = sessionStorage.getItem("token");
+    let query = new URLSearchParams(window.location.search);
+    let movieID = query.get("movieID");
+
+    useEffect(()=>{
+        console.log(movieID)
+    }, [movieID])
 
     return (
         <>
         {!token && <Navigate to="/" />}
-        <h2>Soy el detalle</h2>
+        <h2>Titulo: titulo de la peli</h2>
+        <div className="row">
+            <div className="col-4">
+                imagen de la peli
+            </div>
+            <div className="col-8">
+                <h5>Fecha de estreno:</h5>
+                <h5>Reseña:</h5>
+                <p>El detalle de la pelicula, contame de q se trata</p>
+                <h5>Generos:</h5>
+                <ul>
+                    <li>Genero 1</li>
+                    <li>Genero 2</li>
+                    <li>Genero 3</li>
+                </ul>
+            </div>
+        </div>
         </>
     )
 }
