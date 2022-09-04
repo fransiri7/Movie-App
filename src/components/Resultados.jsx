7a7c720ddd6471f88843ab76d1049100bfd4a89b
fransiri7 <81312079+fransiri7@users.noreply.github.com>
2022-09-04 09:56:34 -0300

    Llamado a la API con palabra clave

--- src/components/Resultados.jsx
+++ src/components/Resultados.jsx
@@ -1,13 +1,67 @@
-function Resultados (){
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import sweetAlert from "@sweetalert/with-react";
+
+function Resultados() {
     let query = new URLSearchParams(window.location.search);
-    let keyword = query.get("pelicula");
+    let keyword = query.get("keyword");
+
+    const [moviesResult, setMoviesResult] = useState([]);
+
+    useEffect(() => {
+        const endpoint = `https://api.themoviedb.org/3/search/movie?api_key=379f6ca71eb017bd6e9f97929ced1fde&language=es-ES&query=${keyword}`;
+        axios
+            .get(endpoint)
+            .then((response) => {
+                const moviesArray = response.data.results;
+                if (moviesArray.length === 0) {
+                    sweetAlert(
+                        <h5>No se encontraron peliculas que coincidan con el nombre buscado</h5>
+                    );
+                } else {
+                    setMoviesResult(moviesArray);
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [moviesResult]);
 
     return (
         <>
-        <h2>Seccion de resultados</h2>
-        <h3>La palabra clave es {keyword}</h3>
+            <h2>
+                Palabra buscada: <em>{keyword}</em>
+            </h2>
+            <div className="row">
+                {moviesResult.map((movie, index) => {
+                    return (
+                        <div className="col-4 my-4" key={index}>
+                            <div className="card">
+                                <img
+                                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                    className="card-img-top"
+                                    alt="..."
+                                />
+                                <div className="card-body">
+                                    <h5 className="card-title">{movie.title}</h5>
+                                    <p className="card-text">
+                                        {movie.overview.substring(0, 100)}...
+                                    </p>
+                                    <Link
+                                        to={`/detalle?movieID=${movie.id}`}
+                                        className="btn btn-primary"
+                                    >
+                                        Detalle
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
         </>
-    )
+    );
 }
 
 export default Resultados;
